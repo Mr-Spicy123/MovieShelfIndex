a019e1b73f0439a8ce03dcecafb1f5e6ab2c6ec9
Mr-Spicy123 <jspic1@ocdsb.ca>
2024-11-16 14:30:23 -0400

    update1

---
+++ index.js
@@ -0,0 +1,509 @@
+var shelf1Back = [
+    "Scooby-doo: Camp Scare",
+    "Bruce Almighty",
+    "Ratatouille",
+    "Batman Begins",
+    "The Pirates: Band of Misfits",
+    "Alvin and the Chipmunks",
+    "Giant 600 Cartoon Collection",
+    "Mulan",
+    "Superbad",
+    "Wedding Crashers",
+    "Oliver & Company",
+    "Monster's Inc.",
+    "Bewitched",
+    "The Fox and the Hound",
+    "I Want a Dog for Christmas, Charlie Brown",
+    "The Princess Bride",
+    "Have A Laugh 2",
+    "Speed 2",
+    "The Dukes of Hazzard",
+    "Bend It Like Beckham",
+    "Alvin and the Chipmunks: The Squeakuel",
+    "World Trade Center",
+    "Old Dogs",
+    "10 Things I Hate About You",
+    "Goobers",
+    "Mary Poppins",
+    "The Book of Life",
+    "Wayne's World",
+    "National Lampoon's Vacation",
+    "Rainbow Brite and the Star Stealer",
+    "The Last Mimzy / Hoot / How To Eat Fried Worms",
+    "Planet 51",
+    "Woody Woodpecker",
+    "Transformers",
+    "Anchorman",
+    "Garfield",
+    "The Perils of Penelope Pitstop",
+    "Inspector Gadget",
+    "XXX",
+    "The Sandlot",
+    "Santa Buddies",
+    "The Fast and the Furious",
+    "Zoolander",
+    "The Hunchback of Notre Dame",
+    "Mac and Me",
+    "Fantasia 2000",
+    "James and the Giant Peach",
+    "Scooby-Doo",
+    "The Beverly Hillbillies",
+    "Buzz Lightyear of Star Command",
+    "The Final Cut"
+]
+
+var shelf1Front = [
+    "102 Dalmatians",
+    "The Lego Movie 2",
+    "Robinhood",
+    "Cinderella",
+    "Betty Boop For President",
+    "Strawberry Shortcake",
+    "Chicago",
+    "The Aristocats",
+    "Paycheck",
+    "The Notebook",
+    "Cast Away",
+    "Edward Scissorhands",
+    "The Rescuers",
+    "Lady and the Tramp",
+    "Fun and Fancy Free",
+    "Diary of a Wimpy Kid: Dog Days",
+    "Shrek The Third",
+    "The Chronicles of Narnia",
+    "E.T.",
+    "Peter Pan",
+    "The Little Mermaid",
+    "Opus n' Bill: A Wish for Wigs That Work",
+    "101 Dalmatians",
+    "Predators From the Deep",
+    "Talladega Nights",
+    "Igor",
+    "Alice in Wonderland",
+    "Corner Gas: Season Two",
+    "American Pie",
+    "The Hunger Games",
+    "Harry Potter and the Order of the Phoenix",
+    "Mars Needs Moms",
+    "Matilda",
+    "Horton Hears a Who!",
+    "Just Friends",
+    "Kalifornia",
+    "Pee Wee's Big Adventure",
+    "It's a Very Merry Muppets Christmas Movie",
+    "Hotel Transilvania",
+    "The Simpsons Movie",
+    "Jungle 2 Jungle",
+    "The Long Way Home",
+    "Bachelor Party Vegas",
+    "Spy Kids: All the Time in the World",
+    "Shorts",
+    "Lilo & Stitch",
+    "Dogs 1",
+    "Encino Man"
+]
+
+var shelf2Back = [
+    "The Hunger Games",
+    "Cars",
+    "The Dark Night",
+    "Men In Black 3",
+    "Transformers: Age of Extinction",
+    "How To Train Your Dragon",
+    "The Hangover",
+    "Tron",
+    "Yogi Bear",
+    "Harry Potter and the Half Blood Prince",
+    "Home Alone 2",
+    "Bedknobs And Broomsticks",
+    "Les Miserables",
+    "The Princess Bride",
+    "Game of Thrones: Season 1",
+    "Game of Thrones: Season 2",
+    "Game of Thrones: Season 3",
+    "The Universe: Season 1",
+    "Independence Day",
+    "Journey to the Edge of the Universe",
+    "Spaceballs",
+    "The Croods",
+    "The Fifth Element",
+    "The Lord of the Rings Trilogy",
+    "Planes",
+    "Clash of the Titans",
+    "Bambi",
+    "Reddick Collection",
+    "Avatar",
+    "2012",
+    "District 9",
+    "309",
+    "Despicable Me 2",
+    "Toy Story 3",
+    "The Hobbit",
+    "The Incredibles",
+    "Cloudy With a Chance of Meatballs",
+    "Rio",
+    "Aladdin",
+    "Alice in Wonderland",
+    "Carstoon",
+    "The Lorax",
+    "Cloudy With a Chance of Meatballs 2",
+    "The Nightmare Before Christmas",
+    "Despicable Me 3",
+    "Wall-E",
+    "Big Hero 6",
+    "Minions",
+    "i, Robot",
+    "The Truman Show",
+    "28 Days Later",
+    "The Lion King"
+]
+
+var shelf2Front = [
+    "Turbo",
+    "Up",
+    "Star Trek",
+    "Final Fantasy 7",
+    "Prometheus",
+    "The Lion King",
+    "Rio 2",
+    "Pete's Dragon",
+    "Hop",
+    "The Lego Movie",
+    "Jack the Giant Slayer",
+    "Maleficent",
+    "Kung Fu Panda 2",
+    "Sleeping Beauty",
+    "Gnomeo & Juliet",
+    "Monsters University",
+    "Beauty and the Beast",
+    "Wreck It Ralph",
+    "Paddington",
+    "Batman Forever",
+    "Transformers: Dark of the Moon",
+    "The Princess and the Frog",
+    "Meet The Robinsons",
+    "Batman & Robin",
+    "The Watch",
+    "Transformers: Revenge of the Fallen",
+    "Cars 2",
+    "The Smurfs 2",
+    "Mr. Peabody and Sherman",
+    "Cats and Dogs",
+    "Dumbo",
+    "Harry Potter and the Deadly Hollows Part 1",
+    "Mr. Popper's Penguins",
+    "Batman",
+    "Diary of a Wimpy Kid",
+    "Harry Potter and the Deadly Hollows Part 2",
+    "Earth To Echo",
+    "Harry Potter: 8 Film Collection",
+    "Total Recall",
+    "Horton Hears a Who",
+    "Tarzan",
+    "Ice Age",
+    "Batman Begins",
+    "Pirates of the Caribbean 3",
+    "Pirates of the Caribbean 2",
+    "Epic",
+    "Pirates of the Caribbean",
+    "The Matrix",
+    "Robinhood: Prince of Thieves",
+    "Frozen",
+    "Brave",
+    "Annie",
+    "Tangled",
+    "Goosebumps",
+    "Spider-Man 3",
+    "Avatar",
+    "Date Night",
+    "Alien",
+    "A Christmas Carol"
+]
+
+var shelf3Back = [
+    "Inside Man",
+    "The Simpsons Christmas",
+    "Gold Rush 2002",
+    "The Adventure of Milo and Otis",
+    "All Dogs Go to Heaven",
+    "Ice Age",
+    "Ewocks",
+    "Ocean's Thirteen",
+    "Brother Bear",
+    "The Jungle Book",
+    "Missing Link",
+    "Extreme Days",
+    "What Happens in Vegas",
+    "Casino Royale",
+    "American Pie 2",
+    "What About Bob",
+    "2 Days in the Valley",
+    "Felix the Cat Saves Christmas",
+    "Super Mario Bros",
+    "Aliens First Christmas",
+    "Hitch",
+    "The Town That Banned Christmas",
+    "Mean Girls",
+    "X-Men Collection",
+    "White Christmas",
+    "The Brave Little Toaster",
+    "Elmo's Christmas Countdown",
+    "Grandma's Boy",
+    "Dodgeball",
+    "Stuart Little",
+    "Toy Story 3",
+    "The Ant Bully",
+    "The Love Guru",
+    "Stuart Little 3",
+    "I Am Legend",
+    "Monsters Vs. Aliens: Mutant Pumpkins",
+    "Napolean Dynamite",
+    "Men In Black 2",
+    "Betty Boop",
+    "Antz",
+    "Super Troopers",
+    "Up",
+    "Chestnut",
+    "The Cat in the Hat",
+    "Two Weeks Notice",
+    "Shrek",
+    "The Family Stone",
+    "Robots",
+    "Caddy Shack 2",
+    "Seabiscuit",
+    "Hotel Rwanda"
+]
+
+var shelf3Front = [
+    "When Zachary Beaver Came to Town",
+    "Harry Potter and the Goblet of Fire",
+    "The Royal Tenenbaums",
+    "American Pie",
+    "Scared Shrekless",
+    "The Legend of Zelda",
+    "Into The Blue",
+    "How to be Single",
+    "Spider-Man",
+    "The Gods Must be Crazy",
+    "Blue Crush",
+    "Napolean Dynamite",
+    "Hoodwinked",
+    "E.T.",
+    "Betty Boop Vol. 1",
+    "You, Me and Dupree",
+    "Ocean's Twelve",
+    "Hercules",
+    "The 40 Year Old Virgin",
+    "Head Over Heels",
+    "Rent",
+    "Pinocchio",
+    "THe Sword in the Stone",
+    "The Matrix",
+    "Monsters Vs. Aliens",
+    "Huckleberry Finn",
+    "Snow White",
+    "The Fox and the Hound 2",
+    "Shera Princess of Power",
+    "The Goonies",
+    "Batman Begins",
+    "Robocop",
+    "The Mask",
+    "Fun With Dick and Jane",
+    "Transformers",
+    "Walk The Line",
+    "Monster-In-Law",
+    "National Treasure",
+    "The Bourne Identity",
+    "The Polar Express",
+    "The Polar Express",
+    "THe Pirates",
+    "Herbie Goes Bananas",
+    "Night of the Museum",
+    "The Pagemaster",
+    "Back to the Future Trilogy",
+    "The Little Rascals",
+    "THe Muppet Christmas Carol",
+    "Harry Potter and the Prisoner of Azkaban",
+    "Yes Man",
+    "The Truman Show",
+    "Forrest Gump",
+    "Night at the Museum",
+    "The Shaggy Dog"
+]
+
+var shelf4Back = [
+    "Enchanted",
+    "The Fox and the Hound",
+    "Munich",
+    "42 - The Jackie Robinson Story",
+    "Dinosaur",
+    "Moving McAllister",
+    "Wrongfully Accused",
+    "61*",
+    "Alvin and the Chipmunks: The Squeak Along",
+    "Madagascar",
+    "Man On Fire",
+    "Road Trip",
+    "Planet Earth",
+    "Ocean's Eleven",
+    "DreamWorks Holiday Collection",
+    "Starwars - The Complete Saga",
+    "Fantastic 4"
+]
+
+var shelf4Front = [
+
+]
+
+var numMovies = shelf1Back.length + shelf1Front.length + shelf2Back.length + shelf2Front.length + shelf3Back.length + shelf3Front.length + shelf4Back.length + shelf4Front.length;
+console.log(numMovies);
+
+for (let i = 0; i < shelf1Back.length; i++) {
+    const container = document.getElementById('back-shelf1');
+    const movie = document.createElement('div');
+    movie.classList.add('movie');
+    movie.id = shelf1Back[i];
+    container.appendChild(movie);
+    console.log(movie.id);
+
+    movie.addEventListener('mouseover', () => {
+        document.getElementById('id-display').textContent = `${movie.id}`;
+    });
+
+    movie.addEventListener('mouseout', () => {
+        //document.getElementById('id-display').textContent = '';
+    });
+}
+
+console.log("");
+
+for (let i = 0; i < shelf1Front.length; i++) {
+    const container = document.getElementById('front-shelf1');
+    const movie = document.createElement('div');
+    movie.classList.add('movie');
+    movie.id = shelf1Front[i];
+    container.appendChild(movie);
+    console.log(movie.id);
+
+    movie.addEventListener('mouseover', () => {
+        document.getElementById('id-display').textContent = `${movie.id}`;
+    });
+
+    movie.addEventListener('mouseout', () => {
+        //document.getElementById('id-display').textContent = '';
+    });
+}
+
+console.log("");
+
+for (let i = 0; i < shelf2Back.length; i++) {
+    const container = document.getElementById('back-shelf2');
+    const movie = document.createElement('div');
+    movie.classList.add('movie');
+    movie.id = shelf2Back[i];
+    container.appendChild(movie);
+    console.log(movie.id);
+
+    movie.addEventListener('mouseover', () => {
+        document.getElementById('id-display').textContent = `${movie.id}`;
+    });
+
+    movie.addEventListener('mouseout', () => {
+        //document.getElementById('id-display').textContent = '';
+    });
+}
+
+console.log("");
+
+for (let i = 0; i < shelf2Front.length; i++) {
+    const container = document.getElementById('front-shelf2');
+    const movie = document.createElement('div');
+    movie.classList.add('movie');
+    movie.id = shelf2Front[i];
+    container.appendChild(movie);
+    console.log(movie.id);
+
+    movie.addEventListener('mouseover', () => {
+        document.getElementById('id-display').textContent = `${movie.id}`;
+    });
+
+    movie.addEventListener('mouseout', () => {
+        //document.getElementById('id-display').textContent = '';
+    });
+}
+
+console.log("");
+
+for (let i = 0; i < shelf3Back.length; i++) {
+    const container = document.getElementById('back-shelf3');
+    const movie = document.createElement('div');
+    movie.classList.add('movie');
+    movie.id = shelf3Back[i];
+    container.appendChild(movie);
+    console.log(movie.id);
+
+    movie.addEventListener('mouseover', () => {
+        document.getElementById('id-display').textContent = `${movie.id}`;
+    });
+
+    movie.addEventListener('mouseout', () => {
+        //document.getElementById('id-display').textContent = '';
+    });
+}
+
+console.log("");
+
+for (let i = 0; i < shelf3Front.length; i++) {
+    const container = document.getElementById('front-shelf3');
+    const movie = document.createElement('div');
+    movie.classList.add('movie');
+    movie.id = shelf3Front[i];
+    container.appendChild(movie);
+    console.log(movie.id);
+
+    movie.addEventListener('mouseover', () => {
+        document.getElementById('id-display').textContent = `${movie.id}`;
+    });
+
+    movie.addEventListener('mouseout', () => {
+        //document.getElementById('id-display').textContent = '';
+    });
+}
+
+console.log("");
+
+for (let i = 0; i < shelf4Back.length; i++) {
+    const container = document.getElementById('back-shelf4');
+    const movie = document.createElement('div');
+    movie.classList.add('movie');
+    movie.id = shelf4Back[i];
+    container.appendChild(movie);
+    console.log(movie.id);
+
+    movie.addEventListener('mouseover', () => {
+        document.getElementById('id-display').textContent = `${movie.id}`;
+    });
+
+    movie.addEventListener('mouseout', () => {
+        //document.getElementById('id-display').textContent = '';
+    });
+}
+
+console.log("");
+
+for (let i = 0; i < shelf4Front.length; i++) {
+    const container = document.getElementById('front-shelf4');
+    const movie = document.createElement('div');
+    movie.classList.add('movie');
+    movie.id = shelf4Front[i];
+    container.appendChild(movie);
+    console.log(movie.id);
+
+    movie.addEventListener('mouseover', () => {
+        document.getElementById('id-display').textContent = `${movie.id}`;
+    });
+
+    movie.addEventListener('mouseout', () => {
+        //document.getElementById('id-display').textContent = '';
+    });
+}
